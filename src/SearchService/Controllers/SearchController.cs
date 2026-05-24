61b5b32576accd0ef77fa73e51ebbd6b02f6fe97
Vanhieu260303 <162543111+Vanhieu260303@users.noreply.github.com>
2026-05-24 16:14:20 +0700

    SearchService and controller search with SearchParams

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Helper;
+using SearchService.Models;
+
+namespace SearchService.Controllers
+{
+    [Route("api/search")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItems(SearchParams searchParams)
+        {
+            var query = DB.PagedSearch<Item,Item>();
+
+            query.Sort(x => x.Ascending(a => a.Make));
+
+            if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+            {
+                query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+            }
+
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+                _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+            };
+
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+            };
+
+            if (!string.IsNullOrEmpty(searchParams.Winner))
+            {
+                query = query.Match(x => x.Winner == searchParams.Winner);
+            }
+
+
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+            {
+                query = query.Match(x => x.Seller == searchParams.Seller);
+            }
+
+
+            query.PageNumber(searchParams.PageNumber);
+            query.PageSize(searchParams.PageSize);
+
+            var result = await query.ExecuteAsync();
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
+        }
+    }
+}
---
+++ src/SearchService/Helper/SearchParams.cs
@@ -0,0 +1,13 @@
+﻿namespace SearchService.Helper
+{
+    public class SearchParams
+    {
+        public string SearchTerm { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string Seller { get; set; }
+        public string Winner { get; set; }
+        public string OrderBy { get; set; }
+        public string FilterBy { get; set; }
+    }
+}
