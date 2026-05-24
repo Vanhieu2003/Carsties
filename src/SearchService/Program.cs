a13f94990b81e4503e6cdc1a98b6d7ae3761ad1e
Vanhieu260303 <162543111+Vanhieu260303@users.noreply.github.com>
2026-05-24 16:49:26 +0700

    add http comunication of get data

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -2,6 +2,7 @@ using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Data;
 using SearchService.Models;
+using SearchService.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
 
 
 var app = builder.Build();
